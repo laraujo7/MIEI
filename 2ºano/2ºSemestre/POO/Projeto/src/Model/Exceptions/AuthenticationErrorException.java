1621cd0a470faa4f6bf18fe38517a4459ce5c0b6
Luis <louis-1996@live.com.pt>
2020-07-05 03:18:58 +0100

    POO & SO

--- 2ºano/2ºSemestre/POO/Projeto/src/Model/Exceptions/AuthenticationErrorException.java
+++
@@ -1,7 +0,0 @@
-package Model.Exceptions;
-
-public class AuthenticationErrorException extends Exception{
-    public AuthenticationErrorException(String s){
-        super(s);
-    }
-}
--- 2ºano/2ºSemestre/POO/Projeto/src/Model/Exceptions/InvalidIDException.java
+++
@@ -1,7 +0,0 @@
-package Model.Exceptions;
-
-public class InvalidIDException extends Exception{
-        public InvalidIDException(String s){
-            super(s);
-        }
-    }
