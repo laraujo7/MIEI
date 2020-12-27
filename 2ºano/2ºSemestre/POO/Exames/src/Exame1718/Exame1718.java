99407cc5285b25ec04781237661aeb3c84141213
Luis <louis-1996@live.com.pt>
2020-12-27 12:45:20 +0000

    addition of Fichas POO

--- 2ºano/2ºSemestre/POO/Exames/src/Exame1718/Exame1718.java
+++ 2ºano/2ºSemestre/POO/Exames/src/Exame1718/Exame1718.java
@@ -1,4 +1,19 @@
 package Exame1718;
 
 public class Exame1718 {
+    //1.a)
+    void adicionaTicket(Ticket t){
+        this.ticketsPorResolver.add(t.clone());
+    }
+
+    //1.b)
+    void resolveTicket(String ident) throws TecnicoNaoExisteException{
+        try{
+            this.ticketsPorResolver.stream().filter(t -> t.getNome0.equals(ident)).
+        }
+        catch(TecnicoNaoExisteException e){
+
+        }
+    }
+
 }
